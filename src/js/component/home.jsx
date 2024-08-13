4aa8b27f753a5e283ba0753f8492472262de26cb
Ericka Castillo <84164725+erickact@users.noreply.github.com>
2024-08-13 01:09:53 +0000

    traffic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,28 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
 
 //create your first component
 const Home = () => {
+	const [ color, setColor] = useState("");
+
+	const handleClick = (selectedColor) => {
+     setColor (selectedColor);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-center align-items-center" style={{height: "100vh"}}>
+			<div className="position-relative">
+				<div className="bg-black position-absolute" style={{width:"10px", height:"80px", top:"-80px", left:"35px"}}></div>
+				<div className="d-flex flex-column align-tems-center justify-content-center align-items-center bg-black rounded" style={{width:"80px", height:"200px"}} >
+					<div className="rounded-circle bg-danger" style={{width:"60px", height:"60px", boxShadow: color === "bg-danger" ? "0px 0px 50px 20px rgb(219,241,190)" : "none"}}
+					onClick={()=>handleClick("bg-danger")}></div>
+					<div className="rounded-circle bg-warning" style={{width:"60px", height:"60px" , boxShadow: color === "bg-warning" ? "0px 0px 50px 20px rgb(219,241,190)" : "none"}}
+					onClick={()=>handleClick("bg-warning")}></div>
+					<div className="rounded-circle bg-success" style={{width:"60px", height:"60px", boxShadow: color === "bg-success" ? "0px 0px 50px 20px rgb(219,241,190)" : "none"}}
+					onClick={()=>handleClick("bg-success")}></div>
+				</div>
+			</div>
 		</div>
+		
 	);
 };
 
